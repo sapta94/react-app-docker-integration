7fe18dd80b03ce6c219c25388a938d061d640350
sapta94 <dey7.kol@gmail.com>
2019-11-20 21:04:47 +0530

    chnaging navbar links

--- src/components/NavbarMenu.js
+++ src/components/NavbarMenu.js
@@ -1,21 +1,17 @@
 import React, { Component } from 'react';
 
-import {Navbar,Nav,FormControl,Form,Button} from 'react-bootstrap';
+import {Navbar,Nav,Button} from 'react-bootstrap';
 
 export default class NavbarMenu extends Component {
     render(){
         return(
             <Navbar bg="light" variant="light">
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Navbar.Brand href="#home">Pollution</Navbar.Brand>
                 <Nav className="mr-auto">
                     <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#features">Features</Nav.Link>
-                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    <Nav.Link href="#features">Recents</Nav.Link>
+                    <Nav.Link href="#pricing">Settings</Nav.Link>
                 </Nav>
-                <Form inline>
-                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                    <Button variant="outline-primary">Search</Button>
-                </Form>
             </Navbar>
         )
     }
