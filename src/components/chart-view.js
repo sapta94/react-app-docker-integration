228c3cb04bc117ca5c25452bd49d7e310a33a120
sapta94 <dey7.kol@gmail.com>
2019-11-22 21:07:21 +0530

    added chart view componenet

---
+++ src/components/chart-view.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class chartView extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  };
+    }
+    render() {
+        return (
+            <div className="row graph-card">
+
+            </div>
+        );
+    }
+}
+
+export default chartView;
